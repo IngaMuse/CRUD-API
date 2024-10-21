aeca6d6e968547219ad999a326abebcf506320b5
IngaMuse <ingamoshkareva@gmail.com>
2024-10-21 12:53:13 +0300

    feat: added horizontal scaling

--- index.ts
+++ index.ts
@@ -5,47 +5,64 @@ import { handlerGetRequests } from "./handlers/handlerGetRequests"
 import { handlerPostRequests } from "./handlers/handlerPostRequests";
 import { handlerPutRequests } from "./handlers/handlerPutRequests";
 import { handlerDeleteRequests } from "./handlers/handlerDeleteRequests";
+import { availableParallelism } from "node:os";
+import cluster from "node:cluster";
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 const args = argv.slice(2);
 const isMulti = !!args.find((arg) => arg === "--multi");
-if (!isMulti) {
-  const server = createServer((request, response) => {
-    try {
-      const { url, method } = request;
+const cores = availableParallelism();
 
-      if (!url?.startsWith("/api/users")) {
-        response.writeHead(404, "Not Found");
-        response.end(JSON.stringify({ message: "Invalid API Endpoint Route" }));
-      } else {
-        switch (method) {
-          case "GET":
-            handlerGetRequests(request, response);
-            break;
-          case "POST":
-            handlerPostRequests(request, response);
-            break;
-          case "PUT":
-            handlerPutRequests(request, response);
-            break;
-          case "DELETE":
-            handlerDeleteRequests(request, response);
-            break;
-          default:
-            response.writeHead(405, "Invalid Request");
-            response.end(
+
+const server = createServer((request, response) => {
+  try {
+    const { url, method } = request;
+
+    if (!url?.startsWith("/api/users")) {
+      response.writeHead(404, "Not Found");
+      response.end(JSON.stringify({ message: "Invalid API Endpoint Route" }));
+    } else {
+      switch (method) {
+        case "GET":
+          handlerGetRequests(request, response);
+          break;
+        case "POST":
+          handlerPostRequests(request, response);
+          break;
+        case "PUT":
+          handlerPutRequests(request, response);
+          break;
+        case "DELETE":
+          handlerDeleteRequests(request, response);
+          break;
+        default:
+          response.writeHead(405, "Invalid Request");
+          response.end(
             JSON.stringify({ message: "Invalid API request method" })
-            );
-            break;
-        }
+          );
+          break;
       }
-    } catch (error) {
-      response.writeHead(500, "Server Error");
-      response.end(JSON.stringify({ message: "Internal Server Error" }));
-    }   
-  });
-  
+    }
+  } catch (error) {
+    response.writeHead(500, "Server Error");
+    response.end(JSON.stringify({ message: "Internal Server Error" }));
+  }
+});
+
+if (cluster.isPrimary) {
   server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Primary. Listen on port ${PORT}`);
+  });
+
+  if (isMulti) {
+    Array.from({ length: cores - 1 })
+      .map((_, index) => index + 1)
+      .forEach((processId) => {
+        cluster.fork({ PORT: PORT + processId });
+      });
+  }
+} else {
+  server.listen(PORT, () => {
+    console.log(`Worker. Listen on port ${PORT}`);
   });
 }
